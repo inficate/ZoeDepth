268aa590bddaa7521fd69441b944d54faf2bb769
Wei Wang <me@mrwei.wang>
2024-02-08 21:00:05 -0800

    Add Gradio app

---
+++ app.py
@@ -0,0 +1,39 @@
+import torch
+from PIL import Image
+from zoedepth.utils.misc import colorize
+import gradio as gr
+
+# Load the model
+model_zoe_n = torch.hub.load(".", "ZoeD_N", source="local", pretrained=True)
+
+# Set device
+DEVICE = "cuda" if torch.cuda.is_available() else "cpu"
+zoe = model_zoe_n.to(DEVICE)
+
+
+def get_depth(input_image):
+    # Convert the input image to RGB
+    image = input_image.convert("RGB")
+
+    # Run inference
+    depth = zoe.infer_pil(image)
+
+    # Colorize output
+    colored = colorize(depth)
+
+    # Convert array to PIL Image
+    output_image = Image.fromarray(colored)
+
+    # Return the colorized output image
+    return output_image
+
+
+# Set up the Gradio interface
+inficate = gr.Interface(
+    fn=get_depth,  # Use the getDepth function
+    inputs=gr.inputs.Image(type="pil", label="Upload Image"),  # Specify input type
+    outputs=gr.outputs.Image(type="pil", label="Depth Image"),  # Specify output type
+)
+
+# Launch the Gradio app
+inficate.launch()
