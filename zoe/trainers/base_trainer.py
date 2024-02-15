e1a9234a7151b0d5be6b94a1802599884824ef48
Wei Wang <me@mrwei.wang>
2024-02-14 22:39:24 -0800

    Rename folder

--- zoe/trainers/base_trainer.py
+++ zoe/trainers/base_trainer.py
@@ -37,8 +37,8 @@ import torch.optim as optim
 import wandb
 from tqdm import tqdm
 
-from zoedepth.utils.config import flatten
-from zoedepth.utils.misc import RunningAverageDict, colorize, colors
+from zoe.utils.config import flatten
+from zoe.utils.misc import RunningAverageDict, colorize, colors
 
 
 def is_rank_zero(args):
@@ -72,7 +72,7 @@ class BaseTrainer:
         import glob
         import os
 
-        from zoedepth.models.model_io import load_wts
+        from zoe.models.model_io import load_wts
 
         if hasattr(self.config, "checkpoint"):
             checkpoint = self.config.checkpoint
